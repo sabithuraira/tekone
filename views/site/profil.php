16332a450990ae245a41901418128571864634da
Sabit Huraira <sabitzhabit@gmail.com>
2018-04-29 09:40:52 +0700

    fix profile

--- views/site/profil.php
+++ views/site/profil.php
@@ -9,15 +9,42 @@ $this->title = 'Profil';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="box box-default">
-<div class="box-header with-border"><h3 class="box-title">
-	<span class="fa fa-tags"></span>
-	Profil Tentang Website</h3>
+	<div class="box-header with-border"><h3 class="box-title">
+		<span class="fa fa-tags"></span>
+		Profil Tentang Website</h3>
 	</div>
-<div class="box-body">	
-<p>Input Fenomena Data PDRB dilakukan oleh admin, pilih sesuai judul variabel dan Kabupaten/Kota yang akan diupload</p>
+
+
+	<div class="box-body">	
     
-	<div class="body-content">
-	<p>Ini adalah . . .</p>
-</div>
-</div>
+	<div class="body-content" style="padding-left: 50px;">
+		<p><big><strong>Forum Temu Ekonomi Triwulanan (For Tekwan)</strong></big></p>
+
+		<hr>
+		<p><big>Forum Temu Ekonomi Triwulanan (<strong>For Tekwan</strong>) adalah proyek perubahan yang merupakan inovasi di Bidang Neraca </big></p>
+
+		<p><big>Wilayah dan Analisis Statistik Badan Pusat Statistik Provinsi Sumatera Selatan yang bertujuan untuk <em>upload</em> data dan </big></p>
+
+		<p><big>fenomena ekonomi secara triwulanan, serta membentuk forum diskusi secara <em>online</em> antara kasi Neraca Produksi dengan </big></p>
+
+		<p><big><em>stakeholder</em> internal dan eksternal.&nbsp;Inovasi ini dibuat untuk mengatasi masalah belum optimalnya penyusunan PDRB Sektoral</big></p>
+
+		<p><big>Triwulanan pada area perubahan bidang Penataan Tata Laksana<strong> </strong>dari sisi efektifitas sistem, proses dan prosedur kerja </big></p>
+
+		<p><big>yang bermanfaat bagi BPS Provinsi Sumatera Selatan dalam penyediaan data pertumbuhan ekonomi triwulanan yang lebih </big></p>
+
+		<p><big>berkualitas.</big></p>
+
+		<p><big>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Aplikasi <strong>For Tekwan</strong> menyediakan fitur bagi pengguna aplikasi untuk melakukan <em>input </em>data dan fenomena, </big></p>
+
+		<p><big>baik yang bersifat <em>regional</em> maupun <em>multiregional</em>.&nbsp; Fitur forum diskusi menjadi bagian terpenting dalam pembahasan </big></p>
+
+		<p><big>pada proses penghitungan angka PDRB Triwulanan antara Tim Penyusun PDRB Provinsi Sumatera Selatan dengan</big></p>
+
+		<p><big>para <em>stakeholder </em>internal maupun eksternal. Hasil akhir dari proyek perubahan ini diharapkan dapat berdampak </big></p>
+
+		<p><big>pada terbangunnya sistem dan prosedur kerja yang efektif dan efisien dalam penyusunan PDRB Sektoral Triwulanan.</big></p>
+
+		</div>
+		</div>
 </div>
